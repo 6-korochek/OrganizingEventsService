eacc97ba0c7bd80a2b0e15c8302466b6e19e3836
SakhnoYA <james.film@mail.ru>
2024-02-09 20:15:20 +0300

    fix settings

--- src/OrganizingEventsService/Program.cs
+++ src/OrganizingEventsService/Program.cs
@@ -4,47 +4,21 @@ using Itmo.Dev.Platform.Common.Extensions;
 using Itmo.Dev.Platform.Logging.Extensions;
 using OrganizingEventsService.Application.Extensions;
 using OrganizingEventsService.Infrastructure.Persistence.Extensions;
+using OrganizingEventsService.Presentation.Http.Extensions;
 
-// using EventMaster.Presentation.Http.Extensions;
-
-// using Microsoft.Extensions.Options;
-// using Newtonsoft.Json;
-// WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-// builder.Configuration.AddUserSecrets<Program>();
-//
-// builder.Services.AddOptions<JsonSerializerSettings>();
-// builder.Services.AddSingleton(sp => sp.GetRequiredService<IOptions<JsonSerializerSettings>>().Value);
-//
-// builder.Services.AddApplication();
-// builder.Services.AddInfrastructurePersistence();
-// builder.Services
-//     .AddControllers()
-//     .AddNewtonsoftJson()
-//     .AddPresentationHttp();
-//
-// builder.Services.AddSwaggerGen().AddEndpointsApiExplorer();
-//
-// builder.Host.AddPlatformSerilog(builder.Configuration);
-// builder.Services.AddUtcDateTimeProvider();
-//
-// WebApplication app = builder.Build();
-//
-// app.UseRouting();
-// app.UseSwagger();
-// app.UseSwaggerUI();
-//
-// app.MapControllers();
-//
-// await app.RunAsync();
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-builder.Services.AddApplication();
-builder.Services.AddInfrastructurePersistence(builder.Configuration);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Host.AddPlatformSerilog(builder.Configuration);
 builder.Services.AddUtcDateTimeProvider();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructurePersistence(builder.Configuration);
+builder.Services
+    .AddControllers()
+    .AddNewtonsoftJson()
+    .AddPresentationHttp();
 
 WebApplication app = builder.Build();
 
@@ -52,3 +26,5 @@ app.UseSwagger();
 app.UseSwaggerUI();
 app.UseRouting();
 app.MapControllers();
+
+await app.RunAsync();
