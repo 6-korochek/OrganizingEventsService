8b4354530651a70190077d0a70641cee58a247a6
AgutinVBoy <68972811+AgutinVBoy@users.noreply.github.com>
2024-03-28 01:17:35 +0300

    Added CQRS AuthController
    Signed-off-by: AgutinVBoy <68972811+AgutinVBoy@users.noreply.github.com>

---
+++ src/Application/OrganizingEventsService.Application/Auth/Commands/LoginCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using OrganizingEventsService.Application.Models.Dto.Account;
+
+namespace OrganizingEventsService.Application.Auth.Commands;
+
+public class LoginCommand : IRequest<AuthenticatedAccountDto>
+{
+    public string Email { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
